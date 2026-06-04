c4f96e0d268797737fb4cb24a254ef56245a6cc3
ZTL-UwU <zhangtianli2006@gmail.com>
2026-06-03 20:23:55 -0700

    chore(edu,lv3): fix background image

--- src/app/screens/education-level/level-3/index.ts
+++ src/app/screens/education-level/level-3/index.ts
@@ -39,7 +39,7 @@ export class EducationSheepScreen extends Container {
     engine().audio.bgm.setVolume(0);
 
     this.background = new Sprite({
-      texture: Texture.from('education-level-3/background.svg'),
+      texture: Texture.from('education-level-3/background.png'),
       layout: { position: 'absolute', width: '100%', height: '100%', objectFit: 'cover' },
     });
     this.hud = new HUD({
