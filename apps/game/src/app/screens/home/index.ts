d52c683c4faf86ee5bc1a7b6b7f2ca908f78acc4
ash160606 <shahaadit2016@gmail.com>
2026-08-11 10:30:23 -0700

    chore(home): update title spelling

--- apps/game/src/app/screens/home/index.ts
+++ apps/game/src/app/screens/home/index.ts
@@ -80,7 +80,7 @@ export class HomeScreen extends Container {
     };
 
     this.title = new Text({
-      text: 'SOZLER SAYLISI',
+      text: 'SOZLER SEYLISI',
       style: {
         fill: 0x284937,
         fontFamily: 'Concert One',
