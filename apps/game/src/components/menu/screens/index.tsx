a2b50ddb502cd43a359c60c3a0ce0ce62b349a7e
Nikita Prabhu <142063028+nikipra16@users.noreply.github.com>
2026-08-18 10:16:22 -0700

    feat(game): about us (#88)

--- apps/game/src/components/menu/screens/index.tsx
+++ apps/game/src/components/menu/screens/index.tsx
@@ -3,11 +3,18 @@ import { CloseButton } from '../../ui/CloseButton';
 import { PrimaryButton } from '../../ui/PrimaryButton';
 
 export interface MenuHomeScreenProps {
-  onAbout: () => void;
-  onReference: () => void;
+  onAlphabet: () => void;
+  onConverter: () => void;
+  onProject: () => void;
+  onTeam: () => void;
 }
 
-export function MenuHomeScreen({ onAbout, onReference }: MenuHomeScreenProps) {
+export function MenuHomeScreen({
+  onAlphabet,
+  onConverter,
+  onProject,
+  onTeam,
+}: MenuHomeScreenProps) {
   return (
     <>
       <CloseButton
@@ -16,8 +23,21 @@ export function MenuHomeScreen({ onAbout, onReference }: MenuHomeScreenProps) {
       />
 
       <div className="flex flex-col items-center gap-8">
-        <PrimaryButton onClick={onAbout}>ABOUT</PrimaryButton>
-        <PrimaryButton onClick={onReference}>REFERENCES</PrimaryButton>
+        <section className="flex w-full flex-col items-center gap-4">
+          <h2 className="font-display text-3xl font-bold tracking-wide text-forest uppercase">
+            About
+          </h2>
+          <PrimaryButton onClick={onProject}>PROJECT</PrimaryButton>
+          <PrimaryButton onClick={onTeam}>TEAM</PrimaryButton>
+        </section>
+
+        <section className="flex w-full flex-col items-center gap-4">
+          <h2 className="font-display text-3xl font-bold tracking-wide text-forest uppercase">
+            References
+          </h2>
+          <PrimaryButton onClick={onAlphabet}>ALPHABET</PrimaryButton>
+          <PrimaryButton onClick={onConverter}>CONVERTER</PrimaryButton>
+        </section>
       </div>
     </>
   );
